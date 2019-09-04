1c0320598d451a0d8faced34d783835ef9ae33a7
gabriel-miranda <gabmirdev@gmail.com>
2019-09-04 00:48:28 -0300

    Fix production build

--- config/index.js
+++ config/index.js
@@ -4,6 +4,6 @@ module.exports = {
   APP_NAME: process.env.APP_NAME || 'cinesilo',
   PORT,
   BASE_URL: process.env.BASE_URL || `http://localhost:${PORT}`,
-  DEV: Boolean(process.env.DEV) || process.env !== 'production',
+  DEV: Boolean(process.env.DEV) || process.env.NODE_ENV !== 'production',
   PAGE_SIZE: parseInt(process.env.PAGE_SIZE, 10) || 10,
 };
